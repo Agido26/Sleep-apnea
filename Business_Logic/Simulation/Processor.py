32035371e2aa3a17523f46b9e8dbdb37fb11090f
Abdelrhman <agido26@gmail.com>
2026-07-29 12:16:57 +0200

    Add proccesing

--- Business_Logic/Simulation/Processor.py
+++ Business_Logic/Simulation/Processor.py
@@ -1,24 +1,25 @@
+import numpy as np
 from scipy.signal import find_peaks
 
 class ECGProcessor:
     @staticmethod
     def calculate_peaks_and_bpm(y_data, fs, height_threshold=0.5, min_distance=40):
-        """
-        دالة تستقبل إشارة الـ ECG الحالية وتقوم بإرجاع أماكن القمم ومعدل نبضات القلب
-        """
         # 1. إيجاد القمم (R-Peaks)
         peaks, _ = find_peaks(y_data, height=height_threshold, distance=min_distance)
         
         bpm = 0
-        # 2. حساب الـ BPM إذا كان لدينا قمتين على الأقل في النافذة الحالية
         if len(peaks) >= 2:
-            last_peak = peaks[-1]
-            previous_peak = peaks[-2]
+            # 2. إيجاد المسافة بالنقاط بين *كل* قمتين متتاليتين في الشاشة
+            # دالة diff تطرح كل رقم في المصفوفة من الرقم الذي يسبقه مباشرة
+            peak_intervals = np.diff(peaks) 
             
-            # المسافة الزمنية بين آخر نبضتين
-            samples_between_peaks = last_peak - previous_peak
-            time_between_beats = samples_between_peaks / fs
+            # 3. حساب متوسط المسافات (للتخلص من تأثير أي قمة وهمية)
+            average_interval_samples = np.mean(peak_intervals)
             
+            # 4. تحويل المتوسط إلى ثواني
+            time_between_beats = average_interval_samples / fs
+            
+            # 5. حساب النبض في الدقيقة
             if time_between_beats > 0:
                 bpm = 60 / time_between_beats
                 
